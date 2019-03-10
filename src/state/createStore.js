dfabefca47fec4e2e129ac65720290ecb36a49a2
handsomekuroji <handsomekuroji@gmail.com>
2019-03-11 02:07:32 +0900

    remove: default state

--- src/state/createStore.js
+++ src/state/createStore.js
@@ -3,14 +3,13 @@ import { createStore as reduxCreateStore } from 'redux'
 const reducer = (state, action) => {
   if (action.type === 'INCREMENT') {
     return Object.assign({}, state, {
-      siteState: state.siteState !== true,
-      buttonState: true
+      siteState: state.siteState !== true
     })
   }
   return state
 }
 
-const initialState = { siteState: false, buttonState: false }
+const initialState = { siteState: false }
 
 const createStore = () => reduxCreateStore(reducer, initialState)
 export default createStore
