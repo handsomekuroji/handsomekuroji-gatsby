74682e476f3dda027cf2485ec17f934d10d5de9c
handsomekuroji <handsomekuroji@gmail.com>
2019-05-03 18:43:05 +0900

    add: 404 page

--- src/components/organisms/header.js
+++ src/components/organisms/header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { media } from '../../components/variable/mixin'
+import { media, font } from '../../components/variable/mixin'
 import Logo from '../atoms/logo'
 import Search from '../atoms/search'
 
@@ -18,6 +18,12 @@ const SiteH1 = styled.h1`
   ${media.ls`width: 240px;`}
 `
 
+const NotFoundH1 = styled.h1`
+  font: bold 1.5rem / 1 ${font.$f_1};
+  margin: 32px 0 0;
+  ${media.ms`margin: 32px 0 48px;`}
+`
+
 const SiteDiv = styled.div`
   width: 60px;
   ${media.ms`width: 180px;`}
@@ -28,7 +34,7 @@ const StyledLink = styled(Link)`
   display: inline-block;
 `
 
-export default function Header({ inIndex }) {
+export default function Header({ inIndex, in404 }) {
   const heading = inIndex ? (
     <SiteH1>
       <Logo />
@@ -39,9 +45,12 @@ export default function Header({ inIndex }) {
     </SiteDiv>
   )
 
+  const notFound = in404 ? <NotFoundH1>404 Not Found</NotFoundH1> : ''
+
   return (
     <SiteHeader>
       <StyledLink to="/">{heading}</StyledLink>
+      {notFound}
       <Search />
     </SiteHeader>
   )
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,71 @@
 import React from 'react'
-import Layout from '../components/layout'
+import { graphql } from 'gatsby'
+import styled from 'styled-components'
+import { media } from '../components/variable/mixin'
 import SEO from '../components/seo'
+import Layout from '../components/layout'
+import Header from '../components/organisms/header'
+import Footer from '../components/organisms/footer'
+import Loop from '../components/organisms/loop'
 
-export default function NotFoundPage() {
+const SiteMain = styled.main`
+  margin: 32px auto 0;
+  max-width: 640px;
+  width: calc(100% - 16px);
+  ${media.xs`
+    width: calc(100% - 32px);
+  `}
+  ${media.s`
+    width: calc(100% - 48px);
+  `}
+  ${media.ms`
+    max-width: 690px;
+    width: calc(100% - 64px);
+  `}
+  ${media.ls`
+    margin: 48px auto 0;
+  `}
+  ${media.l`
+    max-width: 960px;
+  `}
+`
+
+export default function NotFoundPage({ data }) {
   return (
     <Layout>
       <SEO title="404: Not found" />
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Header in404 />
+      <SiteMain>
+        <Loop allPosts={data.allContentfulBlog.edges} />
+      </SiteMain>
+      <Footer alltags={data.allContentfulTag.edges} />
     </Layout>
   )
 }
+
+export const pageQuery = graphql`
+  query NotFound {
+    allContentfulBlog(sort: { fields: [createdAt], order: DESC }, limit: 12) {
+      edges {
+        node {
+          slug
+          title
+          createdAt
+          thumbnail {
+            file {
+              url
+            }
+          }
+        }
+      }
+    }
+    allContentfulTag {
+      edges {
+        node {
+          name
+          slug
+        }
+      }
+    }
+  }
+`
